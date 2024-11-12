ad8c7b9f7b3f90282025b28815f8f4ca68c6ce3e
Michał Dakowicz <michugamesyt0@gmail.com>
2024-11-12 13:42:54 +0000

    py updat

--- matematyka/main.py
+++ matematyka/main.py
@@ -1,24 +1,26 @@
 import os
 import glob
 
-def replace_tg_in_file(file_path):
+def replace(file_path):
     with open(file_path, 'r') as file:
         content = file.read()
 
     content = content.replace("\\tg", "\\mathrm{tg}")
-    content = content.replace("\\ctg", "\\mathrm{ctg}")
     content = content.replace("\\operatorname{tg}", "\\mathrm{tg}")
+
+    content = content.replace("\\ctg", "\\mathrm{ctg}")
     content = content.replace("\\operatorname{ctg}", "\\mathrm{ctg}")
+
     content = content.replace("\\Z", "\\mathbb{Z}")
     content = content.replace("\\R", "\\mathbb{R}")
 
     with open(file_path, 'w') as file:
         file.write(content)
 
-def replace_tg_in_folder(folder_path):
+def replace_folder(folder_path):
     for file_path in glob.glob(os.path.join(folder_path, '**', '*.*'), recursive=True):
         if file_path.endswith('.md'):
-            replace_tg_in_file(file_path)
+            replace(file_path)
 
 if __name__ == "__main__":
-    replace_tg_in_folder(os.path.dirname(os.path.abspath(__file__)))
+    replace_folder(os.path.dirname(os.path.abspath(__file__)))
